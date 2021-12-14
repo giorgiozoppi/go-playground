208fa259dad42e6d5d82d90ab47d7185e12494db
Giorgio Zoppi <giorgio.zoppi@gmail.com>
2021-12-14 11:45:52 +0100

    Generator design pattern. 	new file:   adapter/example.go 	new file:   adapter/sammysang.go 	new file:   adapter/sammysangAdapter.go 	new file:   adapter/sohnee.go 	new file:   adapter/television.go 	new file:   builder/builder.go 	new file:   builder/example.go 	new file:   builder/notification.go 	new file:   facade/cafe.go 	new file:   facade/coffeemachine.go 	new file:   facade/example.go 	new file:   generator/main.go 	new file:   iterator/books.go 	new file:   iterator/example.go 	new file:   iterator/iterable.go 	new file:   observer/example.go 	new file:   observer/observer.go 	new file:   observer/subject.go

---
+++ design_patterns/generator/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func foo() <-chan string {
+	mychannel := make(chan string)
+
+	go func() {
+		for i := 0; ; i++ {
+			mychannel <- fmt.Sprintf("%s %d", "Counter at : ", i)
+		}
+	}()
+
+	return mychannel // returns the channel as returning argument
+}
+
+func main() {
+	mychannel := foo() // foo() returns a channel.
+
+	for i := 0; i < 5; i++ {
+		fmt.Printf("%q\n", <-mychannel)
+	}
+	fmt.Println("Done with Counter")
+}
