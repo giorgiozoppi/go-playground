ae1c0f3cf3c866408a3a6325a54af40a82893883
Giorgio Zoppi <giorgio.zoppi@gmail.com>
2021-12-20 20:22:06 +0100

    new file:   ../stack/stack.go 	new file:   ../stack/stack_test.go

---
+++ stack/stack_test.go
@@ -0,0 +1,37 @@
+package slist
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateLoopedList(t *testing.T) {
+	root := NewListNode(3)
+	second := NewListNode(5)
+	third := NewListNode(8)
+	fourth := NewListNode(10)
+	root.Next = second
+	second.Next = third
+	third.Next = fourth
+	fourth.Next = second
+	item := HasCycle(root)
+	require.Equal(t, item, true)
+}
+func TestCreateSingleList(t *testing.T) {
+	root := NewListNode(10)
+	root.AddTail(20)
+	root.AddTail(30)
+	require.Equal(t, root.Data, 10)
+	require.Equal(t, root.Next.Data, 20)
+	require.Equal(t, root.Next.Next.Data, 30)
+
+}
+func TestSearchSingleList(t *testing.T) {
+	root := NewListNode(20)
+	root.AddTail(20)
+	root.AddTail(30)
+	node := root.Search(20)
+	require.NotEqual(t, node, nil)
+	require.Equal(t, node.Data, 20)
+}
